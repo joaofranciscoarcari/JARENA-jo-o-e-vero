c965b8367671c9536c15776dacd46dd10fe4f525
Verônica Antonini Martini <veronica.martini@estudante.uffs.edu.br>
2026-04-12 18:11:11 -0300

    Update AgentesFortoes.java
    Comandos basicos necessarios para qualquer operação inclusos ( estrategia ainda nao inclusa)
    possiveis alteraçoes vao ser feitas nesses comandos (visto que coloquei apenas como base para iniciar o codigo)

--- src/br/uffs/cc/jarena/AgentesFortoes.java
+++ src/br/uffs/cc/jarena/AgentesFortoes.java
@@ -1,5 +1,41 @@
+//Trabalho JARENA de programação orientada a objetos
+
+//Integrantes: Veronica Antonini Martini (Turma do professor Andrei) e Joao Francisco Arcari (Turma do professor Geomar)
+//Nome da equipe: Agentes Fortoes
+
 package br.uffs.cc.jarena;
 
-public class AgentesFortoes {
-    
+public class AgentesFortoes extends Agente{
+
+public AgentesFortoes(Integer x, Integer y, Integer energia) {
+	super(x, y, energia);
+	setDirecao(geraDirecaoAleatoria());}
+
+    	
+	public void pensa() {
+		if(!podeMoverPara(getDirecao())) {
+			setDirecao(geraDirecaoAleatoria());
+		}
+		
+		if(podeDividir() && getEnergia() >= 800) {
+			divide();
+		}
+	}
+	
+	public void recebeuEnergia() {
+	}
+	
+	public void tomouDano(int energiaRestanteInimigo) {
+	}
+	
+	public void ganhouCombate() {
+        
+    }
+	
+	public void recebeuMensagem(String msg) {
+	}
+	
+	public String getEquipe() {
+		return "Agentes Fortoes";
+	}
 }
